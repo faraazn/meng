added divide module to consolidate spectrogram/rgb normalization within sequential, fixed video file extension to be webm

pytorch-a2c-ppo-acktr-gail/a2c_ppo_acktr/model.py
@@ -14,6 +14,14 @@ class Flatten(nn.Module):
             x = x.contiguous()
         return x.view(x.size(0), -1)
 
+class Divide(nn.Module):
+    def __init__(self, divisor):
+        super(Divide, self).__init__()
+        self.divisor = divisor
+
+    def forward(self, x):
+        return x / self.divisor
+
 class Policy(nn.Module):
     def __init__(self, obs_space, obs_module, action_space, base_kwargs=None):
         super(Policy, self).__init__()
@@ -186,6 +194,7 @@ class NNBase2(NNBase):
                 # My original video model - 942,080 parameters
                 num_inputs = obs_space[obs_name].shape[0]  # should be 3, RGB
                 module = nn.Sequential(  # video shape [3, 224, 320]
+                    Divide(255.0),  # input range [0, 255] -> [0, 1]
                     nn.BatchNorm2d(num_inputs),
                     init_(nn.Conv2d(num_inputs, 32, 8, stride=4)), nn.ReLU(),  # [32, 55, 79]
                     init_(nn.Conv2d(32, 64, 4, stride=4)), nn.ReLU(),  # [64, 13, 19]
@@ -196,6 +205,7 @@ class NNBase2(NNBase):
                 # OpenAI Baselines small - 8,104,960 parameters
                 num_inputs = obs_space[obs_name].shape[0]  # should be 3, RGB
                 module = nn.Sequential(  # video shape [3, 224, 320]
+                    Divide(255.0),  # input range [0, 255] -> [0, 1]
                     nn.BatchNorm2d(num_inputs),
                     init_(nn.Conv2d(num_inputs, 16, 8, stride=4)), nn.ReLU(),  # [16, 55, 79]
                     init_(nn.Conv2d(16, 32, 4, stride=2)), nn.ReLU(),  # [32, 26, 38]
@@ -205,20 +215,17 @@ class NNBase2(NNBase):
                 # OpenAI Baselines large - 28,387,328 parameters
                 num_inputs = obs_space[obs_name].shape[0]  # should be 3, RGB
                 module = nn.Sequential(  # video shape [3, 224, 320]
-                    #nn.BatchNorm2d(num_inputs),
+                    Divide(255.0),  # input range [0, 255] -> [0, 1]
                     init_(nn.Conv2d(num_inputs, 32, 8, stride=4)), nn.ReLU(),  # [32, 55, 79]
-                    #nn.BatchNorm2d(32), nn.ReLU(),
                     init_(nn.Conv2d(32, 64, 4, stride=2)), nn.ReLU(),  # [64, 26, 38]
-                    #nn.BatchNorm2d(64), nn.ReLU(),
                     init_(nn.Conv2d(64, 64, 3, stride=1)), nn.ReLU(),  # [64, 24, 36]
-                    #nn.BatchNorm2d(64), nn.ReLU(),
                     Flatten(), init_(nn.Linear(64*24*36, 512)), nn.ReLU())  # [512]
                 #self.output_size += 512
             elif obs_module[obs_name] == 'audio-small':
                 # My original audio model - 65,536 parameters
                 module = nn.Sequential(  # audio shape [735,]
                     MelSpectrogram(sample_rate=22050, n_fft=512, hop_length=735//2+1),  # [128, 2]
-                    Flatten(), nn.BatchNorm1d(256),  # [256]
+                    Divide(80.0), Flatten(),  # [256]
                     init_(nn.Linear(128*2, 256)), nn.ReLU())  # [256]
                 #self.output_size += 256
             else:

pytorch-a2c-ppo-acktr-gail/visualize/scorecam.py
@@ -23,29 +23,23 @@ class CamExtractor():
             Does a forward pass on convolutions, hooks the function at given layer
         """
         conv_outputs = {}
-        x = []
+        all_x = []
         for obs_name in obs.keys():
             if obs_name == 'video':
                 module = self.actor_critic.base.video_module
-                # TODO: make this nicer by including divide in module
-                vid_x = obs['video'] / 255
-                for m_pos, m in module._modules.items():
-                    vid_x = m(vid_x)
-                    if obs_name in self.obs_target.keys() and self.obs_target[obs_name] == int(m_pos):
-                        conv_outputs[obs_name] = vid_x
-                x.append(vid_x)
             elif obs_name == 'audio':
                 module = self.actor_critic.base.audio_module
-                aud_x = obs['audio']
-                for m_pos, m in module._modules.items():
-                    aud_x = m(aud_x)
-                    if obs_name in self.obs_target.keys() and self.obs_target[obs_name] == m_pos:
-                        conv_outputs[obs_name] = aud_x
-                x.append(aud_x)
             else:
                 raise NotImplementedError
-        x = torch.cat(x, dim=1)
-        return conv_outputs, x
+            
+            x = obs[obs_name]
+            for m_pos, m in module._modules.items():
+                x = m(x)
+                if obs_name in self.obs_target.keys() and self.obs_target[obs_name] == int(m_pos):
+                    conv_outputs[obs_name] = x
+            all_x.append(x)
+        all_x = torch.cat(all_x, dim=1)
+        return conv_outputs, all_x
 
     def forward_pass(self, x):
         """